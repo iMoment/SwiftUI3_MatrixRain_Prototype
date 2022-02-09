3976ed200cd6c196061f9c01c9142255e8f03f89
Stanley Pan <imoment.developer@gmail.com>
2022-02-09 15:18:05 -0500

    Initial Commit

---
+++ MatrixRain/MatrixRainApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatrixRainApp.swift
+//  MatrixRain
+//
+//  Created by Stanley Pan on 2022/02/09.
+//
+
+import SwiftUI
+
+@main
+struct MatrixRainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
