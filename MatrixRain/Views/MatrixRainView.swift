27393427acc8e15b92b156b347f44f3c761af211
Stanley Pan <imoment.developer@gmail.com>
2022-02-09 15:37:15 -0500

    Added assets, MatrixRainView, base project setup

---
+++ MatrixRain/Views/MatrixRainView.swift
@@ -0,0 +1,20 @@
+//
+//  MatrixRainView.swift
+//  MatrixRain
+//
+//  Created by Stanley Pan on 2022/02/09.
+//
+
+import SwiftUI
+
+struct MatrixRainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MatrixRainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MatrixRainView()
+    }
+}
